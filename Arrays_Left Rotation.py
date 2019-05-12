130e8b26b7a99698c7aa922b3a8b5da88a38105a
Shane Smullen <shanesmullen@gmail.com>
2019-05-12 15:57:41 +0100

    Add files via upload

---
+++ Arrays_Left Rotation.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    
+    for i in range (d):
+        over = a[0]
+        for i in range(len(a)-1):
+            a[i] = a[i+1]
+        a[len(a) -1] = over
+    
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
