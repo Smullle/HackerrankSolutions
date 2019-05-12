446f5cd5fdffaa09c5fb840f7ef3c34e6e84e1e7
Shane Smullen <shanesmullen@gmail.com>
2019-05-12 11:54:44 +0100

    Add files via upload

---
+++ 2DArray-DS.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    
+    length = len(arr)
+    sum = 0
+    maxSum = 0
+    
+    for i in range(length -2):
+        for j in range(length -2):
+            sum = (arr[i][j] + arr[i][j+1] + arr[i][j+2] + \
+                       arr[i+1][j+1] + \
+                       arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+
+            if(sum > maxSum):
+                maxSum = sum    
+    
+    return maxSum
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
